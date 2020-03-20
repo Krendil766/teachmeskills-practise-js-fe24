4f5930d730ec0b1d3a11473208c5853818a03d8c
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-20 20:55:10 +0300

    feat: one, two

--- lesson-02/exercise-01.spec.js
+++ lesson-02/exercise-01.spec.js
@@ -1,82 +1,85 @@
 describe('a Promise represents an operation that hasn`t completed yet, but is expected in the future', function() {
-  it('`Promise` is a global function', function() {
-    const expectedType = '???';
+    it('`Promise` is a global function', function() {
+        const expectedType = 'function';
 
 
-    chai.expect(typeof Promise).to.equal(expectedType);
-  });
-
-
-
-  describe('simplest promises', function() {
-    it('resolve a promise by calling the `resolve` function given as first parameter', function(done) {
-      let promise = new Promise((resolve) => {
-
-      });
-      promise
-        .then(() => done())
-        .catch(() => done(new Error('The promise is expected to resolve.')));
+        chai.expect(typeof Promise).to.equal(expectedType);
     });
 
 
 
-    it('the `resolve` function can return a value, that is consumed by the `promise.then()` callback', function(done) {
-      let promise = new Promise((resolve) => {
-        resolve();
-      });
+    describe('simplest promises', function() {
+        it('resolve a promise by calling the `resolve` function given as first parameter', function(done) {
+            let promise = new Promise((resolve) => {
+                resolve();
+            });
+            promise
+                .then(() => done())
+                .catch(() => done(new Error('The promise is expected to resolve.')));
+        });
 
 
-      promise
-        .then(value => {chai.expect(value).to.equal(42); done(); })
-        .catch(() => done(new Error('The promise is expected to resolve with 42!')));
+
+        it('the `resolve` function can return a value, that is consumed by the `promise.then()` callback', function(done) {
+            let promise = new Promise((resolve) => {
+                resolve(42);
+            });
+
+
+            promise
+                .then(value => {
+                    chai.expect(value).to.equal(42);
+                    done();
+                })
+                .catch(() => done(new Error('The promise is expected to resolve with 42!')));
+        });
+
+
+
+        it('rejecting a promise is done by calling the callback given as 2nd parameter', function(done) {
+            let promise = new Promise((resolve, reject) => {
+                reject();
+            });
+
+            promise
+                .then(() => done(new Error('The promise is expected to be rejected.')))
+                .catch(() => done());
+        });
     });
 
 
 
-    it('rejecting a promise is done by calling the callback given as 2nd parameter', function(done) {
-      let promise = new Promise(() => {
-        reject();
-      });
+    describe('an asynchronous promise', function() {
+        it('can resolve later, also by calling the first callback', function(done) {
+            let promise = new Promise((resolve) => {
+                setTimeout(() => resolve(), 100);
+            });
+            promise
+                .then(() => done())
+                .catch(() => done(new Error('The promise is expected to resolve.')));
+        });
 
-      promise
-        .then(() => done(new Error('The promise is expected to be rejected.')))
-        .catch(() => done());
+
+
+        it('reject it at some later point in time, calling the 2nd callback', function(done) {
+            let promise = new Promise((resolve, reject) => {
+                setTimeout(() => reject(), 100);
+            });
+            promise
+                .then(() => done(new Error('The promise is expected to be rejected.')))
+                .catch(() => done());
+        });
     });
-  });
 
 
 
-  describe('an asynchronous promise', function() {
-    it('can resolve later, also by calling the first callback', function(done) {
-      let promise = new Promise(() => {
-        setTimeout(() => resolve(), 100);
-      });
-      promise
-        .then(() => done())
-        .catch(() => done(new Error('The promise is expected to resolve.')));
+    describe('test library (mocha here) support for promises', function() {
+        it('just returning the promise makes the test library check that the promise resolves', function() {
+            let promise = new Promise((resolve, reject) => {
+                resolve();
+            });
+            // return the promise to mocha, it has the checking for promise resolving built in, when it receives a promise
+            return promise;
+        });
     });
-
-
-
-    it('reject it at some later point in time, calling the 2nd callback', function(done) {
-      let promise = new Promise((reject) => {
-        setTimeout(() => reject(), 100);
-      });
-      promise
-        .then(() => done(new Error('The promise is expected to be rejected.')))
-        .catch(() => done());
-    });
-  });
-
-
-
-  describe('test library (mocha here) support for promises', function() {
-    it('just returning the promise makes the test library check that the promise resolves', function() {
-      let promise = new Promise((reject, resolve) => {
-        resolve();
-      });
-      // return the promise to mocha, it has the checking for promise resolving built in, when it receives a promise
-      return promise;
-    });
-  });
 });
