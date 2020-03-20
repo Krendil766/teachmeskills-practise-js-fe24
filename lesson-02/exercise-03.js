de580badd101563f55e96862d9059b20f1308cfb
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-20 21:47:30 +0300

    feat:task 3

--- lesson-02/exercise-03.js
+++ lesson-02/exercise-03.js
@@ -1,6 +1,27 @@
-// Exercise 03:
-//Create an object called shape that has the type property and a getType() method.
+// Создаем объект с именем shape, который имеет свойство type и метод getType ().
 
-//Define a Triangle() constructor function whose prototype is shape. Objects created with Triangle() should have three own properties—a, b, and c, representing the lengths of the sides of a triangle.
+// Определяем функцию конструктора Triangle (), прототип которой - shape. Объекты, созданные с помощью Triangle (), должны иметь три собственных свойства - a, b и c, представляющих длины сторон треугольника.
 
-//Add a new method to the prototype called getPerimeter()
+// Добавить новый метод в прототип с именем getPerimeter ()
+
+function Triangle(a, b, c) {
+    this.a = a
+    this.b = b
+    this.c = c
+
+
+}
+
+let shape = {
+    type: 'triangle',
+    getType: function() {
+        return this.type
+    },
+    __proto__: new Triangle
+
+
+}
+Triangle.prototype = shape;
+Triangle.prototype.getPerimeter = function() {
+    return this.a + this.b + this.c
+}
